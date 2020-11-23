e1e2ddc712627f025b37961c5f3d62836563cd59
YunusStahlschmidt <48024253+YunusStahlschmidt@users.noreply.github.com>
2020-11-23 23:27:47 +0300

    Add files via upload

--- myprog4.sh
+++ myprog4.sh
@@ -6,25 +6,27 @@ then
 	#Checks whether the given input file exists
 	if [ -f "./$1" ]
 	then
+		touch tmp.txt
 		#read file with while loop with IFS character by character
 		# -N1 allows to read character by character including \n
 		while IFS= read -N1 c
 		do 
 			#checking caharacter if it is a number or not
 			case $c in
-				1)printf "one";;
-				2)printf "two";;
-				3)printf "three";;
-				4)printf "four";;
-				5)printf "five";;
-				6)printf "six";;
-				7)printf "seven";;
-				8)printf "eight";;
-				9)printf "nine";;
-				0)printf "zero";;
-				*)printf "$c";;
+				1) echo -n "one" >> tmp.txt;;
+				2) echo -n "two" >> tmp.txt;;
+				3) echo -n "three" >> tmp.txt;;
+				4) echo -n "four" >> tmp.txt;;
+				5) echo -n "five" >> tmp.txt;;
+				6) echo -n "six" >> tmp.txt;;
+				7) echo -n "seven" >> tmp.txt;;
+				8) echo -n "eight" >> tmp.txt;;
+				9) echo -n "nine" >> tmp.txt;;
+				0) echo -n "zero" >> tmp.txt;;
+				*) echo -n "$c" >> tmp.txt;;
 			esac			
 		done < $1
+		mv tmp.txt $1
 	else
 	echo "File does not exist!"
 	fi
