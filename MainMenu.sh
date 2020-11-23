06d94fa05f4dd403c4cb74c033dc32b780f43679
Sameeh Kunbargi <48989642+SamNK@users.noreply.github.com>
2020-11-23 20:16:00 +0300

    Update MainMenu.sh

--- MainMenu.sh
+++ MainMenu.sh
@@ -4,23 +4,38 @@
 clear
 while true
     do
-        clear
         echo "Please select an option:"
         echo
-        echo "1. Create historgram"
-        echo "2. Encryption"
-        echo "3. Delete oldest"
-        echo "4. Convert numbers"
-        echo "5. Organize files"
-        echo "6. Exit"
+        echo "1. Create historgram : enter a filename"
+        echo "2. Encryption : enter a String and an Integer"
+        echo "3. Delete oldest : empty or a pathname"
+        echo "4. Convert numbers : enter a filename"
+        echo "5. Organize files : wild card argument and -R option"
+        echo "6. Exit"  
         echo -n
         read yourch
         case $yourch in 
-            1) source ./prog1.sh; read;;
-            2) ./prog2.sh;;
-            3) source ./myprog3.sh;;
-            4) ./prog4.sh;;
-            5) ./prog5.sh;;
+            1) 
+            	echo "enter a filename"
+        	read args1;
+        	source ./myprog1.sh $args1;;
+            2) 
+            	echo "enter a String and an Integer"
+        	read args2;
+        	source ./myprog2.sh $args2;;
+            3) 
+            	echo "enter a filename that you want to delete"
+        	read args3;
+        	source ./3.sh $args3;;
+            4) 
+            	echo "enter a filename"
+        	read args4;
+        	source ./myprog4.sh $args4;;
+            5) 
+            	echo "enter a wildcard and optinal -R option"
+        	read args5;
+        	source ./prog5.sh $args5;;
+
             6) exit 0;;
             *) echo -n "Please select a valid choice! (1-6)"; read;;
         esac
